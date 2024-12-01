b9d0c40742466f871525b0ec6cd4c5e581f0dbe5
Eduarda Oliveira <duda_silva.oliveira@hotmail.com>
2024-12-01 17:28:28 -0300

    feat: adicionando teste de api-mensagens

---
+++ cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,14 @@
+describe('Api Adopet', () => {
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/f0175d50-7ee1-4783-9a25-56a7550b16b0',
+            headers: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+        })
+    })
+})
