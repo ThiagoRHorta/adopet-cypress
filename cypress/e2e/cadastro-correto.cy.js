5d25e6130935c7e747ff89db6bf616abce346927
ThiagoRHorta <thiagorhorta@gmail.com>
2024-12-01 21:03:17 -0300

    b

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -2,7 +2,7 @@ describe('Página de Cadastro', () => {
   it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app');
     cy.get('[data-test="register-button"]').click();
-    cy.cadastrar('Thiago Rodrigues Horta', 'thiagorhorta5@email.com', 'Senha123', 'Senha123')
+    cy.cadastrar('Thiago Rodrigues Horta', 'thiagorhorta6@email.com', 'Senha123', 'Senha123')
     cy.get('[data-test="submit-button"]').click();
   })
 })
